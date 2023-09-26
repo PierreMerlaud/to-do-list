8fe8f4fa1a8c259b1c10772e50586b7f8de0ae29
pierre merlaud <pmerlaud007@gmail.com>
2023-09-26 14:42:56 +0200

    app base done before coding in ts

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <div className="mainDiv">
+        <h1 className="mainDivH1">To-do-list</h1>
+        <p className="mainDivText">
+          Une to-do-list c&apos;est vraiment super hihi !
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
